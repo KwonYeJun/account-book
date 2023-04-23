6a6515f69a0f873c5f09bd1b8e62a0559afc19e3
gwonyejun <kyjuni1059@gmail.com>
2023-04-23 19:58:55 +0900

    서버 응답 요청 모듈화

--- src/ctrl/JS/server.js
+++ src/ctrl/JS/server.js
@@ -1,26 +1,65 @@
 import http from "http";
 import db from "../../../util/db/dbclass.js";
+import fs from "fs";
+function page(url,res) {
+  if (url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    const htmlfs = fs.readFileSync("./src/views/HTML/login.html", "utf8");
+    res.write(htmlfs);
+    res.end();
+  }
+  if(url === include('login.js')){
+    res.writeHead(200, { "Content-Type": "text/html" });
+    const htmlfs = fs.readFileSync("./src/views/JS/login.js", "utf8");
+    res.write(htmlfs);
+    res.end();
+  }
+  if(url === include('login.css')){
+    res.writeHead(200, { "Content-Type": "text/html" });
+    const htmlfs = fs.readFileSync("./src/views/CSS/login.css", "utf8");
+    res.write(htmlfs);
+    res.end();
+  }
+  if(url === "/register"){}
+}
+
 const server = http.createServer((req, res) => {
-  if (req.method === "GET") {
-    if (req.url === "/") {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write("<h1>Hello World</h1>");
-      const testdb = new db("localhost", "root", "1234", "testdb");
-      res.end(
-        console.log(
-          testdb.query("desc new_table", (err, rows) => {
-            if (err) {
-              console.log(err);
-            } else {
-              console.log(rows);
-            }
-          })
-        )
-      );
-    }
+  try{
+  switch (req.method) {
+    case "GET":
+      page(req.url,res);
+      break;
+    case "POST":
+      page(req.url);
+      break;
   }
-  if (req.method === "POST") {
+}
+  catch(err){
+    console.log('서버 응답 안 받아');
   }
+  // if (req.method === "GET") {
+  //   if (req.url === "/") {
+  //     res.writeHead(200, { "Content-Type": "text/html" });
+  //     const htmlfs = fs.readFileSync("./src/views/HTML/login.html", "utf8");
+  //     res.write(htmlfs);
+  //     res.end();
+  //   }
+      // const testdb = new db("localhost", "root", "1234", "testdb");
+      // res.end(
+      //   console.log(
+      //     testdb.query("desc new_table", (err, rows) => {
+      //       if (err) {
+      //         console.log(err);
+      //       } else {
+      //         console.log(rows);
+      //       }
+      //     })
+      //   )
+      // );
+    
+  
+  // if (req.method === "POST") {
+  // }
 });
 
 server.listen(2222, (error) => {
