2e541f2a0bf37d589c148d22806a831dbf2bf332
gwonyejun <kyjuni1059@gmail.com>
2023-05-07 15:21:47 +0900

    절대 경로 상대경로 미지정, 클라이언트 사이드와 서버 사이드에 대해 알아보자

--- src/ctrl/JS/expressTest.js
+++ src/ctrl/JS/expressTest.js
@@ -1,15 +1,26 @@
 import express from 'express';
-
+import fs from 'fs';
+import path from 'path';
 // ! Express 애플리케이션 생성하기
 const app = express();
 
 
 // ! 라우팅 설정하기
 console.log(app);
-app.get('/', (req, res) => {
-  res.send('heelong')
-})
+// app.get('/', (req, res) => {
+//   const mainPage = fs.readFileSync("./src/views/HTML/login.html", "utf8");
+//   res.send(mainPage);
+// })
 
+// app.get('/', (req, res) => {
+//   const filePath = path.join(__dirname, '/src/views/HTML/login.html');
+//   res.sendFile(filePath);
+// });
+app.get('/', (req, res) => {
+  const filePath = path.join(__dirname, 'src', 'views', 'HTML', 'login.html');
+  const mainPage = fs.readFileSync(filePath, 'utf-8');
+  res.send(mainPage);
+})
 app.listen(2222, () => {
   console.log('성공 2222번호 포크')
 })
