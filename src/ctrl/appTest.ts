5dcafdf2a1c353c3ef93f61bea92b4892f5ff387
gwonyejun <kyjuni1059@gmail.com>
2023-05-25 10:09:27 +0900

    POST데이터 받아 오기 연습

--- src/ctrl/appTest.ts
+++ src/ctrl/appTest.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response ,NextFunction} from "express";
+import express, { Request, Response, NextFunction } from "express";
 import * as path from "path";
 
 const root = path.join(__dirname, "../../")
@@ -10,13 +10,13 @@ const app = express();
 
 // 메인 페이지를 연결해 주기 위하며 처음 접속을 도와주는 미들 웨워 하나와 같이 파일을 정적으로 연결을 해주었다.
 app.get("/", (req: Request, res: Response) => {
-  const filePath = path.join(root,'/src/views/html/login.html');
+  const filePath = path.join(root, '/src/views/html/login.html');
   res.sendFile(filePath);
 });
 app.use(express.static(path.join(root, "/src/views")));
 
 
-const myMiddleware = (req : Request, res : Response, next : NextFunction) => {
+const myMiddleware = (req: Request, res: Response, next: NextFunction) => {
   console.log("미들웨어 함수가 실행되었습니다.");
   next(); // 다음 미들웨어로 제어를 전달합니다.
 };
@@ -24,6 +24,27 @@ const myMiddleware = (req : Request, res : Response, next : NextFunction) => {
 app.use(myMiddleware);
 // 다음으로 실행될 미들 웨어는 웹 브라우저에 제공할 정적 파일들이 위치한 경로입니다.
 app.use(express.static(root));
+
+
+// POST 데이터를 파싱하기 위한 미들웨어
+app.use(express.urlencoded({ extended: true })); 
+// JSON 형식의 POST 데이터를 파싱하기 위한 미들웨어
+app.use(express.json()); 
+
+app.post('/data', (req: Request, res: Response) => {
+  const postData = req.body; // POST 데이터는 req.body에서 접근 가능합니다.
+  // 데이터 처리 로직 작성
+  res.send('데이터가 성공적으로 전송되었습니다.');
+});
+
+app.post('/', (req : Request, res: Response) => {
+
+
+
+});
+
+
+
 
 
 app.listen(2222, () => {
