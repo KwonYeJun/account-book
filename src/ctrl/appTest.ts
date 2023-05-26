546e500ea17aa2a908339eb83849ec1e947701eb
gwonyejun <kyjuni1059@gmail.com>
2023-05-26 18:07:28 +0900

    express Post값 받아보기

--- src/ctrl/appTest.ts
+++ src/ctrl/appTest.ts
@@ -1,12 +1,17 @@
 import express, { Request, Response, NextFunction } from "express";
 import * as path from "path";
-
+import * as bodyParser from "body-parser";
 const root = path.join(__dirname, "../../")
 console.log(root)
 
 
 
 const app = express();
+
+
+// POST 데이터를 파싱하기 위한 미들웨어
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // 메인 페이지를 연결해 주기 위하며 처음 접속을 도와주는 미들 웨워 하나와 같이 파일을 정적으로 연결을 해주었다.
 app.get("/", (req: Request, res: Response) => {
@@ -25,24 +30,19 @@ app.use(myMiddleware);
 // 다음으로 실행될 미들 웨어는 웹 브라우저에 제공할 정적 파일들이 위치한 경로입니다.
 app.use(express.static(root));
 
+// app.use(express.urlencoded({ extended: true })); 
 
-// POST 데이터를 파싱하기 위한 미들웨어
-app.use(express.urlencoded({ extended: true })); 
-// JSON 형식의 POST 데이터를 파싱하기 위한 미들웨어
-app.use(express.json()); 
+// // JSON 형식의 POST 데이터를 파싱하기 위한 미들웨어
+// app.use(express.json()); 
 
-app.post('/data', (req: Request, res: Response) => {
+
+
+app.post('/', (req: Request, res: Response) => {
   const postData = req.body; // POST 데이터는 req.body에서 접근 가능합니다.
   console.log(postData);
   // 데이터 처리 로직 작성
   res.send('데이터가 성공적으로 전송되었습니다.');
   
-});
-
-app.post('/', (req : Request, res: Response) => {
-
-
-
 });
 
 
