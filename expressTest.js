dc773b58d0b27ce6d83a358f1dca3979e476fd63
gwonyejun <kyjuni1059@gmail.com>
2023-05-30 12:00:09 +0900

    React 태그로 라이브러리로 불러와 사용을 해본다.

--- expressTest.js
+++ expressTest.js
@@ -1,26 +1,29 @@
-import express from 'express';
-import fs from 'fs';
-import path from 'path';
-// ! Express 애플리케이션 생성하기
-const app = express();
+// import express from 'express';
+// import fs from 'fs';
+// import path from 'path';
+// // ! Express 애플리케이션 생성하기
+// const app = express();
 
 
-// ! 라우팅 설정하기
-console.log(app);
+// // ! 라우팅 설정하기
+// console.log(app);
+// // app.get('/', (req, res) => {
+// //   const mainPage = fs.readFileSync("./src/views/HTML/login.html", "utf8");
+// //   res.send(mainPage);
+// // })
+
+// // app.get('/', (req, res) => {
+// //   const filePath = path.join(__dirname, '/src/views/HTML/login.html');
+// //   res.sendFile(filePath);
+// // });
 // app.get('/', (req, res) => {
-//   const mainPage = fs.readFileSync("./src/views/HTML/login.html", "utf8");
+//   const filePath = path.join(__dirname, 'src', 'views', 'HTML', 'login.html');
+//   const mainPage = fs.readFileSync(filePath, 'utf-8');
 //   res.send(mainPage);
 // })
+// app.listen(2222, () => {
+//   console.log('성공 2222번호 포크')
+// })
 
-// app.get('/', (req, res) => {
-//   const filePath = path.join(__dirname, '/src/views/HTML/login.html');
-//   res.sendFile(filePath);
-// });
-app.get('/', (req, res) => {
-  const filePath = path.join(__dirname, 'src', 'views', 'HTML', 'login.html');
-  const mainPage = fs.readFileSync(filePath, 'utf-8');
-  res.send(mainPage);
-})
-app.listen(2222, () => {
-  console.log('성공 2222번호 포크')
-})
+const a = new Date();
+console.log(a);
---
+++ test/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function testApp() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>This is a simple React testApp.</p>
+    </div>
+  );
+}
+
+export default testApp;
