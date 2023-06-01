e6e9a88e827190c2f2667672a17da31cc3d30130
gwonyejun <kyjuni1059@gmail.com>
2023-06-01 09:14:31 +0900

    dev서버 경로

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
-import './login.css'; // CSS 파일 경로
+import './views/CSS/login.css'; // CSS 파일 경로
 
 import Test from './Testbutton';
 const rootElement = document.getElementById('root');
